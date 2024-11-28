e9b862d86e2ab322df50721f67ddb5edf068e1de
krinal micropillar <krinalmicropillar@gmail.com>
2024-11-28 14:56:17 +0530

    Set Dynamic Category section in home page

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,43 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NewslatterComponent } from '../../commonComponent/newslatter/newslatter.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { InstagramComponent } from '../../commonComponent/instagram/instagram.component';
+import { LoginServices } from '../../Services/login.services';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NewslatterComponent,InstagramComponent, SlickCarouselModule, FormsModule, CommonModule],
+  imports: [NewslatterComponent, InstagramComponent, SlickCarouselModule, FormsModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  Allcategory: any[] = [];
+  paginatedCategories: any[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 3; 
+  totalPages: number = 1;
+  dots: number[] = []; 
+  autoScrollInterval: any; 
+  slickConfig: any;
 
-  bestSeller: any[] = [
-    {
-      src: 'assets/images/skin.png',
-      title: 'Skin Care'
-    },
-    {
-      src: 'assets/images/hair.png',
-      title: 'Hair Care'
-    },
-    {
-      src: 'assets/images/oils.png',
-      title: 'Essential Oils'
-    },
-    {
-      src: 'assets/images/baby.png',
-      title: 'Baby Care'
-    },
-    {
-      src: 'assets/images/man.png',
-      title: " Man's Grooming"
-    },
-    {
-      src: 'assets/images/cosmetic.png',
-      title: 'Color Cosmetic'
-    },
-  ]
+  constructor(
+    private loginServices: LoginServices,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.GetAll();
+    this.autoScrollInterval = setInterval(() => {
+      this.nextPage();
+    }, 3000); 
+  }
+
+  ngOnDestroy(): void {
+    if (this.autoScrollInterval) {
+      clearInterval(this.autoScrollInterval);
+    }
+  }
+
+  GetAll(){
+    this.loginServices.getall().subscribe((res: any) => {
+        if (res.isSuccess) {
+          this.Allcategory = res.httpResponse;
+          this.totalPages = Math.ceil(this.Allcategory.length / this.itemsPerPage);
+          this.updatePaginatedItems();
+        } else {
+          this.toastr.error(res.message);
+        }
+      });
+  }
+
+  updatePaginatedItems(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedCategories = this.Allcategory.slice(startIndex, endIndex);
+    this.dots = new Array(this.totalPages).fill(0);
+  }
+
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.updatePaginatedItems();
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    } else {
+      this.currentPage = 1; 
+    }
+    this.updatePaginatedItems();
+  }
 }
